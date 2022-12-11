4ac0a22c3a91c95cc8776f9539447e0a7fe6e0bb
Mikhail Samin <i@mishasamin.xyz>
2022-12-11 23:05:01 +0000

    Update remember_scroll.js

--- static/js/remember_scroll.js
+++ static/js/remember_scroll.js
@@ -1,31 +1,24 @@
 function confirm(text, callback) {
-    // add a question inplace of navbar__brand
     var brand = document.querySelector('.navbar__brand');
     var question = document.createElement('div');
     question.classList.add('navbar__brand');
-    // remember the original brand
     question.setAttribute('data-original-brand', brand.innerHTML);
     question.innerHTML = text;
     brand.parentNode.insertBefore(question, brand);
     brand.style.display = 'none';
-    // add buttons
     var buttons = document.createElement('div');
     buttons.classList.add('navbar__brand');
     buttons.innerHTML = '<button class="button button--primary" style="margin-right:5px;">Да</button><button class="button button--secondary">Нет</button>';
     brand.parentNode.insertBefore(buttons, brand);
     buttons.querySelector('button').addEventListener('click', function() {
-        // remove the navbar__brand div, show the original brand (a href navbar__brand)
         question.parentNode.removeChild(question);
         brand.style.display = 'flex';
-        // remove the buttons
         buttons.parentNode.removeChild(buttons);
         callback(true);
     });
     buttons.querySelector('button:last-child').addEventListener('click', function() {
-        // remove the navbar__brand div, show the original brand (a href navbar__brand)
         question.parentNode.removeChild(question);
         brand.style.display = 'flex';
-        // remove the buttons
         buttons.parentNode.removeChild(buttons);
         callback(false);
     });
@@ -57,12 +50,6 @@ function onDocumentReady() {
                 }
             });
         }
-        // on scroll, save the scroll position
-        window.addEventListener('scroll', function() {
-            var currentPage = document.URL.split("/book/")[1];
-            localStorage.setItem("hpmor-scroll-page_"+currentPage, document.documentElement.scrollTop);
-            localStorage.setItem("hpmor-last-page", currentPage);
-        });
     }
 
     if(document.URL == 'https://xn--c1asakg.xn--p1ai/') {
@@ -84,6 +71,13 @@ function onDocumentReady() {
 
 if (document.readyState !== 'loading') {
     onDocumentReady();
+    // on scroll, save the scroll position
+    window.addEventListener('scroll', function() {
+        if(!document.URL.includes("/book/1/")) return;
+        var currentPage = document.URL.split("/book/")[1];
+        localStorage.setItem("hpmor-scroll-page_"+currentPage, document.documentElement.scrollTop);
+        localStorage.setItem("hpmor-last-page", currentPage);
+    });
 } else {
     document.addEventListener('DOMContentLoaded', onDocumentReady);
 }
