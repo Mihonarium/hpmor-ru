1ae0108714c888de8591418b7171642693432c68
Mikhail Samin <i@mishasamin.xyz>
2022-12-14 16:58:53 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,6 +34,7 @@ module.exports = {
         src: 'https://xn--c1asakg.xn--p1ai/img/favicon.ico',
       },
       items: [
+        {href: 'https://xn--c1asakg.xn--p1ai/book', label: 'Читать', position: 'right'},
         {href: 'https://xn--c1asakg.xn--p1ai/%D0%BE-%D0%BA%D0%BD%D0%B8%D0%B3%D0%B5-%D0%B8-%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4%D0%B5/', label: 'О книге и переводе', position: 'right'},
         {href: 'https://xn--c1asakg.xn--p1ai/%D0%B0%D1%83%D0%B4%D0%B8%D0%BE%D0%BA%D0%BD%D0%B8%D0%B3%D0%B0/', label: 'Аудиокнига', position: 'right'},
         {href: 'https://lesswrong.ru/forum/index.php?board=3.0', label: 'Форум', position: 'right'},
