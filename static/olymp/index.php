49ea6464d3d52877a72ea5809a438cd67ae48754
Mikhail Samin <i@mishasamin.xyz>
2022-12-17 13:05:50 +0000

    Create index.php

---
+++ static/olymp/index.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>Redirecting to https://гпмрм.рф/getbooks/?olymp</title>
+<meta http-equiv="refresh" content="0; URL=https://xn--c1asakg.xn--p1ai/getbooks/?olymp">
+<link rel="canonical" href="https://xn--c1asakg.xn--p1ai/getbooks/?olymp">
