51c896159f7e0fb468a0c027905a229c334c2952
Mikhail Samin <i@mishasamin.xyz>
2023-01-26 17:40:18 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -110,11 +110,11 @@ const config = {
             },
             {
               label: 'О книге и переводе',
-              href: '/%D0%BE-%D0%BA%D0%BD%D0%B8%D0%B3%D0%B5-%D0%B8-%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4%D0%B5/',
+              href: '/о-книге-и-переводе/',
             },
             {
               label: 'Аудиокнига',
-              href: '/%D0%B0%D1%83%D0%B4%D0%B8%D0%BE%D0%BA%D0%BD%D0%B8%D0%B3%D0%B0/',
+              href: '/аудиокнига/',
             },
             {
               label: 'Форум',
