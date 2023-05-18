2935b6e9733afcb25757c900d97bb91d862ad307
Mikhail Samin <i@mishasamin.xyz>
2023-05-18 13:58:36 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -116,7 +116,7 @@ function Home() {
 				"bestRating":"5",
 				"worstRating":"1"
 			},
-			"sameAs": "https://www.wikidata.org/wiki/Q22162553"
+			"sameAs": "https://ru.wikipedia.org/wiki/%D0%93%D0%B0%D1%80%D1%80%D0%B8_%D0%9F%D0%BE%D1%82%D1%82%D0%B5%D1%80_%D0%B8_%D0%BC%D0%B5%D1%82%D0%BE%D0%B4%D1%8B_%D1%80%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B3%D0%BE_%D0%BC%D1%8B%D1%88%D0%BB%D0%B5%D0%BD%D0%B8%D1%8F"
 		})}
 	</script>
 	  </Head>
