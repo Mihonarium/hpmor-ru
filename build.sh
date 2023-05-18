ddd9785e308ab29099b4ffc1923888eeef98518e
Mikhail Samin <i@mishasamin.xyz>
2023-05-18 14:31:06 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-# !/bin/bash
+#!/bin/bash
 
 npm run build
 
 find . -name 'index.html' -mindepth 2 -type f -exec sh -c '
     parent="$(dirname "$1")";
     mv "$1" "$parent/../$(basename "$parent").html";
-' find-sh {}
+' find-sh {} \;
