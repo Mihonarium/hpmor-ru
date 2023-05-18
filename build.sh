3644b7631ab0904f978ce7602ea05059c939ef8c
Mikhail Samin <i@mishasamin.xyz>
2023-05-18 14:24:24 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+npm run build
+
+find . -name 'index.html' -mindepth 2 -type f -exec sh -c '
+    parent="$(dirname "$1")";
+    mv "$1" "$parent/../$(basename "$parent").html";
+' find-sh {}
