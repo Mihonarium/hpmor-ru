f2546b1bf0a08b4583f93ee19d3db5ccbb0ff6e2
Mikhail Samin <i@mishasamin.xyz>
2023-05-22 12:16:16 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 npm run build
 
-find . -name 'index.html' -mindepth 2 -type f -not -path "./build/index.html" -exec sh -c '
+find . -name 'index.html' -mindepth 2 -type f -not -path "./build/index.html" -not -path "./build/getbooks/*" -exec sh -c '
     parent="$(dirname "$1")";
     mv "$1" "$parent/../$(basename "$parent").html";
 ' find-sh {} \;
