1ad1c23c3bb43162ef85de3a0bd7d9433061fed3
Mikhail Samin <i@mishasamin.xyz>
2023-06-27 12:25:57 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
         {href: '/book/', label: 'Читать', position: 'right'},
         {href: 'https://гпмрм.рф/%D0%BE-%D0%BA%D0%BD%D0%B8%D0%B3%D0%B5-%D0%B8-%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4%D0%B5', label: 'О книге и переводе', position: 'right'},
         {href: 'https://гпмрм.рф/%D0%B0%D1%83%D0%B4%D0%B8%D0%BE%D0%BA%D0%BD%D0%B8%D0%B3%D0%B0', label: 'Аудиокнига', position: 'right'},
-        {href: 'https://lesswrong.ru/forum/index.php?board=3.0', label: 'Форум', position: 'right'},
+        {href: 'https://t.me/hpmor_chat', label: 'Обсудить', position: 'right'},
         {
           label: 'Скачать', position: 'right',
           items: [
